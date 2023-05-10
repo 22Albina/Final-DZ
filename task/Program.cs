8a05918ca09bdd20fa7346d0b02914a9135ea60d
Albina <drozdowaalbina@yandex.ru>
2023-05-10 19:07:38 +0300

    Добавили схему

--- task/Program.cs
+++ task/Program.cs
@@ -1,5 +1,15 @@
-﻿string[] array1 = new string[] {"11111", "222", "33", "qwer","heh", "tak", "array"};
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. 
+
+
+
+
+
+string[] array1 = new string[] {"11111", "222", "33", "qwer","heh", "tak", "array"};
 string[] array2 = new string[array1.Length];
+
 void StringArray(string[] array1, string[] array2)
 {
     int count = 0;
@@ -12,6 +22,7 @@ void StringArray(string[] array1, string[] array2)
         }
     }
 }
+
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
